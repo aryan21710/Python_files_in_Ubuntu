48deaab696e446fd191d49d1485a560d4013b920
aryan21710 <aryan21710>
2018-03-07 19:10:58 +0530

    ADDED 2 FILES 1ST TIME

---
+++ increase_size_of_a_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys
+import os.path
+
+
+with open('a.txt','w') as f:
+    
+     print ('INITIAL SIZE OF THE FILE :- {}'.format(os.path.getsize('a.txt')))
+     usersize=int(input('ENTER THE SIZE OF THE FILE U WANT TO CREATE:-'))
+     if os.path.isfile('a.txt'):
+        for i in range(0,usersize+1):
+            if os.path.getsize('a.txt')<usersize:
+               f.write(str(i)+'\n')
+               #print('NEW SIZE OF THE FILE:- {}'.format(os.path.getsize('a.txt')))
+
+
+print('FINAL SIZE OF THE FILE:- {}'.format(os.path.getsize('a.txt')))
