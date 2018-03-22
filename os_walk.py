ce055588908f6ab2af7b8b590715de71bbbfd321
aryan21710 <aryan21710>
2018-03-22 18:46:21 +0530

    PRINT STATEMENTS MORE READABLE

--- os_walk.py
+++ os_walk.py
@@ -13,11 +13,11 @@ else:
           print ('\n'*2)
           print ('+'*60)
           print ('The root of the DIr Structure:-',root)
-          print ('No of Dir Inside it:-',len(dirs))
-          print ('The Dir Are:-', dirs)
+          print ('Found "{}" SubDirectories:-'.format(len(dirs)))
+          print ('List of SUbDirectories Are:-', dirs)
           for filename in files:
-              print ('No of Files Are:-',len(files)) 
-              print ('The Files Are:-#',i,'"',os.path.join(root,filename),'"')
+              print ('No of Files Found in this Dir:-',len(files)) 
+              print ('List of Files:-#',i,'"',os.path.join(root,filename),'"')
               if os.path.isfile(os.path.join(root,filename)):
                  i+=1
                  if i==10:
